e90d3610492d1567c070a01a3d6ea45d83e71c02
CHANDRU K <chandrukuppuswamy@outlook.com>
2017-11-19 17:45:25 +0530

    fundamentals check in
    fundamentals check in

---
+++ 02-Fundamentals/02-Fundamentals/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Fundamentals
+{
+    class Program
+    {
+        public class Developer
+        {
+            public string Name;
+            public string Language;
+            public int Age;
+        }
+
+        static void Main(string[] args)
+        {
+            Developer[] developers = new Developer[] {
+                                        new Developer {Name = "Paolo", Language = "C#"},
+                                        new Developer {Name = "Marco", Language = "C#"},
+                                        new Developer {Name = "Frank", Language = "VB.NET"}
+            };
+            var developersUsingCSharp =
+                                          from d in developers
+                                          where d.Language == "C#"
+                                          select d.Name;
+            foreach (var item in developersUsingCSharp)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ 02-Fundamentals/FromInLinq/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FromInLinq
+{
+    class Program
+    {
+        // Simple Entity Classes
+
+        public class Customer
+        {
+            public String Name { get; set; }
+            public String City { get; set; }
+            public Order[] Orders { get; set; }
+        }
+        public class Order
+        {
+            public Int32 IdOrder { get; set; }
+            public Decimal EuroAmount { get; set; }
+            public String Description { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            queryWithJoin();
+
+            Console.ReadLine();
+
+        }
+        /// <summary>
+        /// Simple method Illustrate to have 2 from Clauses in the LINQ statement
+        /// </summary>
+        static void queryWithJoin()
+        {
+            Customer[] customers = new Customer[] {
+            new Customer {
+                Name = "Paolo", City = "Brescia",
+                Orders = new Order[]
+                {
+                    new Order { IdOrder = 1, EuroAmount = 100, Description = "Order 1" },
+                    new Order { IdOrder = 2, EuroAmount = 150, Description = "Order 2" },
+                    new Order { IdOrder = 3, EuroAmount = 230, Description = "Order 3" },
+              }
+            },
+            new Customer {
+                Name = "Marco", City = "Torino",
+                Orders = new Order[] {
+                    new Order { IdOrder = 4, EuroAmount = 320, Description = "Order 4" },
+                    new Order { IdOrder = 5, EuroAmount = 170, Description = "Order 5" },
+            }
+            } };
+            var ordersQuery =
+            from c in customers
+            from o in c.Orders
+            select new { c.Name, o.IdOrder, o.EuroAmount };
+            foreach (var item in ordersQuery)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
