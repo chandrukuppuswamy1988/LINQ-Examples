0f3c052d02369a962623c03e3ee8c23418e4dc4c
CHANDRU K <chandrukuppuswamy@outlook.com>
2017-11-19 18:37:36 +0530

    Group changes
    Group changes

--- 02-Fundamentals/02-Fundamentals/02-Fundamentals/Program.cs
+++ 02-Fundamentals/02-Fundamentals/02-Fundamentals/Program.cs
@@ -12,28 +12,85 @@ namespace _02_Fundamentals
         {
             public string Name;
             public string Language;
-            public int Age;
+            public int Age { get; set; }
         }
+
+        static Developer[] developers = new Developer[] {
+                    new Developer { Name = "Paolo", Language = "C#", Age = 32 },
+                    new Developer { Name = "Marco", Language = "C#", Age = 37},
+                    new Developer { Name = "Frank", Language = "VB.NET", Age = 48 },
+            };
+
+
 
         static void Main(string[] args)
         {
-            Developer[] developers = new Developer[] {
-                                        new Developer {Name = "Paolo", Language = "C#"},
-                                        new Developer {Name = "Marco", Language = "C#"},
-                                        new Developer {Name = "Frank", Language = "VB.NET"}
-            };
+
+            GroupAnoynomousType();
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Simple Usage
+        /// </summary>
+        static void simpleUsage()
+        {
+
             var developersUsingCSharp =
                                           from d in developers
                                           where d.Language == "C#"
                                           select d.Name;
+
             foreach (var item in developersUsingCSharp)
             {
                 Console.WriteLine(item);
             }
 
-            Console.ReadLine();
+    
+        }
 
+
+        /// <summary>
+        /// Group In LINQ SIMPLE Example
+        /// </summary>
+        static void GroupSimpleUsage()
+        {
+            var developersGroupedByLanguage =
+                                                from d in developers
+                                                group d by d.Language;
+
+            foreach (var group in developersGroupedByLanguage)
+            {
+                Console.WriteLine("Language: {0}", group.Key);
+                foreach (var item in group)
+                {
+                    Console.WriteLine("\t{0}", item.Name);
+                }
+            }
+
+            
 
         }
+
+        /// <summary>
+        /// Group by item with anoynomous type (AgeCluster)
+        /// </summary>
+        static void GroupAnoynomousType()
+        {
+            var developersGroupedByLanguage =
+                                              from d in developers
+                                              group d by new { d.Language, AgeCluster = (d.Age / 10) * 10 };
+
+            foreach (var group in developersGroupedByLanguage)
+            {
+                Console.WriteLine("Language: {0}", group.Key);
+                foreach (var item in group)
+                {
+                    Console.WriteLine("\t{0}", item.Name);
+                }
+            }
+
+        }
+
+
     }
 }
--- 02-Fundamentals/02-Fundamentals/FromInLinq/Program.cs
+++ 02-Fundamentals/02-Fundamentals/FromInLinq/Program.cs
@@ -54,7 +54,6 @@ namespace FromInLinq
             queryWithJoin();
 
             // QueryExpressionWithWhereClause();
-
             Console.ReadLine();
 
         }
