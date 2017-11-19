29e09d21ddb09ee98973e98c88a0095ed772d4cf
CHANDRU K <chandrukuppuswamy@outlook.com>
2017-11-19 16:37:31 +0530

    Basic Usage of LINQ
    Basic Usage of LINQ

---
+++ 01-BasicUsage/01-BasicUsage/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_BasicUsage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ShowLargefilesWithLinq(@"C:\Windows");
+            Console.ReadLine();
+
+        }
+        /// <summary>
+        /// Shows the top 5 largest files in the given path 
+        /// </summary>
+        /// <param name="path"></param>
+        public static void ShowLargefilesWithLinq(string path)
+        {
+            var query = from file in new DirectoryInfo(path).GetFiles()
+                        orderby file.Length descending
+                        select file;
+
+            foreach(var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name ,-20} : {file.Length}");
+            }
+
+        }
+    }
+}
